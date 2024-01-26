2059523211b9f3a2c576aaa74b3fbcf7fb0abe96
Kartik Agrawal <Kartik.Agrawal.19723@gmail.com>
2024-01-26 22:55:24 +0530

    Start learning JS

---
+++ Experiment/JavaScript/index.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    console.log("Hello", name);
+}
+let person = {
+    he: "Kartik",
+    age: 20
+};
+let array = ["he", 20];
+greet(person.he)
+console.log(person["age"]);
+console.log("Hi", array);
